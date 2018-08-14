081c7c5aee68e49862fc77a4885570288be5474e
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-08-13 20:25:44 -0400

    fix postgres error

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_09_171150) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "message"
